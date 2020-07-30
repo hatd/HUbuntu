89187d73cf1f4dddb760dc1a53445addd50fee4e
hatd <trandanhha2694@gmail.com>
2020-07-30 14:53:29 +0700

    add function update_bash

--- scripts/ubuntu_install.sh
+++ scripts/ubuntu_install.sh
@@ -5,6 +5,14 @@ initializer(){
   sudo apt-get install -y curl git
 }
 
+update_bash(){
+  # git_complete="https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash"
+
+  # sudo curl $git_complete > /etc/bash_completion.d/git-completion.bash
+  cp -b bash/bash_aliases ~/.bash_aliases
+}
+
 run_all(){
   initializer
+  update_bash
 }
